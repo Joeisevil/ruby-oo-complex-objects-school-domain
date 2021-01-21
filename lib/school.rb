a19668a494fc9e69e3815fe6bed4f737dfbb6aa3
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-01-20 19:58:47 -0500

    commit tree 749dac483b4aa42d5646d2fbe175ca0e191bd045

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+    attr_accessor :roster, :student_name, :school_name, :grade
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+        @student_name
+    end
+    
+    def add_student(student_name, grade)
+        if @roster[grade] 
+            @roster[grade] << student_name
+        else
+            @roster[grade] =[]
+            @roster[grade] << student_name
+        end
+   
+    end
+    
+    def grade(grade)
+        #return all students in that grade in an array
+        @roster[grade]
+    end
+
+    def sort
+        hash = {}
+        @roster.sort.each do |grade, student_name|
+            hash[grade] = (student_name.sort)
+        end
+        hash
+    end
+
+   
+end
